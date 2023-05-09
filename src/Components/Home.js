4e9fb99f04c11dd0d9abf16142a7cd6871af4ebe
Bhavika <bhavikashah8@gmail.com>
2023-05-09 10:42:16 +0530

    Changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,9 @@
-import React from "react";
-import BannerBackground from "../Assets/home-banner-background.png";
+import React from 'react';
+import BannerBackground from '../Assets/home-banner-background.png';
 import AboutBackgroundImage from '../Assets/about-background-image.png';
 
-import Navbar from "./Navbar";
-import { FiArrowRight } from "react-icons/fi";
+import Navbar from './Navbar';
+import { FiArrowRight } from 'react-icons/fi';
 
 const Home = () => {
   return (
@@ -14,15 +14,13 @@ const Home = () => {
           <img src={BannerBackground} alt="" />
         </div>
         <div className="home-text-section">
-          <h1 className="primary-heading">
-            Buy Products
-          </h1>
+          <h1 className="primary-heading">Buy Products</h1>
           <p className="primary-text">
-            Healthy switcher chefs do all the prep work, like peeding, chopping
-            & marinating, so you can cook a fresh food.
+            No middleman here! Our farm-fresh milk is delivered straight to your
+            door for the freshest taste possible!
           </p>
           <button className="secondary-button">
-            Order Now <FiArrowRight />{" "}
+            Order Now <FiArrowRight />
           </button>
         </div>
         <div className="home-image-section">
